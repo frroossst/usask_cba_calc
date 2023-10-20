debca2e0b1066affa4d839515176be13e07135ff
Adhyan H <adhyanpatel@gmail.com>
2023-10-20 03:44:07 -0600

    0.2.0 - input via stdin or filepath

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
-use grade_calculator::ingest::*;
+use usask_cba_calc::ingest::*;
+use std::io::{self, Read};
+
+
 
 fn main() {
-    let subjects = read_and_parse_file("./grades.json".to_string());
+    let args: Vec<String> = std::env::args().collect();
+
+    let subjects;
+
+    if args.len() == 2 {
+        // If exactly one argument is provided, treat it as a file path.
+        let file_path = &args[1];
+        subjects = read_and_parse_file(file_path.to_string());
+    } else {
+        // If no or more than one argument is provided, read from stdin (piped input).
+        let mut input_data = String::new();
+        io::stdin().read_to_string(&mut input_data).expect("Failed to read from stdin");
+        subjects = read_and_parse_file(input_data);
+    }
 
     println!("{:#?}", subjects);
 
