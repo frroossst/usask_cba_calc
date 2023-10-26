74e440d0090dc0005953ae824fbba99340b5a222
Adhyan H <adhyanpatel@gmail.com>
2023-10-26 02:10:02 -0600

    change each text arg to take in colour arg

--- src/schema.rs
+++ src/schema.rs
@@ -1,3 +1,4 @@
+use ansi_term::{Colour, Style};
 use serde_json::{Map, Value};
 use std::io::{self, Write};
 use std::fs::File;
@@ -8,10 +9,26 @@ fn input() -> String {
     return input_buffer.trim().to_string();
 }
 
-fn prompt(str_to_print: &str, arg: Option<&str>) {
-    match arg {
-        Some(s) => print!("[{}] {}", s, str_to_print),
-        None => print!("{}", str_to_print),
+enum PrintSettings {
+    Style(Style),
+    Colour(Colour),
+}
+
+struct PrintArgs<'a> {
+    str_to_print: &'a str,
+    arg: Option<&'a str>,
+    colour: Option<PrintSettings>,
+}
+
+fn prompt(args: PrintArgs) {
+    let PrintArgs { str_to_print, arg, colour } = args;
+    match (arg, colour) {
+        (Some(arg), Some(PrintSettings::Colour(colour))) => print!("[{}] {} ", colour.paint(arg), colour.paint(str_to_print)),
+        (Some(arg), Some(PrintSettings::Style(style))) => print!("[{}] {} ", style.paint(arg), style.paint(str_to_print)),
+        (None, Some(PrintSettings::Colour(colour))) => print!("{}", colour.paint(str_to_print)),
+        (None, Some(PrintSettings::Style(style))) => print!("{}", style.paint(str_to_print)),
+        (Some(arg), None) => print!("[{}] {} ", arg, str_to_print),
+        (None, None) => print!("{}", str_to_print),
     }
     io::stdout().flush().unwrap();
 }
@@ -20,7 +37,11 @@ pub fn construct_schema() {
     let mut schema = Map::new();
 
     loop {
-        prompt("Enter subject name or 'q' to quit: ", None);
+        prompt(PrintArgs { 
+            str_to_print: "Enter subject name or 'q' to quit: ", 
+            arg: None, 
+            colour: Some(PrintSettings::Style(Style::new().underline().fg(Colour::Yellow))),
+        });
         let subject_name = input();
         if subject_name == "q" {
             break;
@@ -29,7 +50,11 @@ pub fn construct_schema() {
         let mut clos = Map::new();
 
         loop {
-            prompt("Enter CLO name (1.1, 3.2 etc.) or 'q' to go back to subjects: ", Some(&subject_name));
+            prompt(PrintArgs { 
+                str_to_print: "Enter CLO name (1.1, 3.2 etc.) or 'q' to go back to subjects: ", 
+                arg: Some(&subject_name), 
+                colour: Some(PrintSettings::Colour(Colour::Cyan)) 
+            });
             let clo_name = input();
             if clo_name == "q" {
                 break;
@@ -38,7 +63,11 @@ pub fn construct_schema() {
             let mut rlos = Map::new();
 
             loop {
-                prompt("Enter RLO name (1.1, 3.2 etc.) or 'q' to go back to CLOs: ", Some(&clo_name));
+                prompt(PrintArgs { 
+                    str_to_print: "Enter 'q' to go back to CLOs: ", 
+                    arg: Some(&clo_name), 
+                    colour: Some(PrintSettings::Colour(Colour::Cyan))
+                });
                 let rlo_name = input();
                 if rlo_name == "q" {
                     break;
@@ -63,12 +92,20 @@ pub fn construct_schema() {
     let default_filename = "grades.json";
 
     if std::path::Path::new(default_filename).exists() {
-        prompt("File already exists. Overwrite? (y/n): ", None);
+        prompt(PrintArgs { 
+            str_to_print: "File already exists. Overwrite? (y/n): ", 
+            arg: None,  
+            colour: Some(PrintSettings::Colour(Colour::Cyan)) 
+        });
         let overwrite = input();
         if overwrite == "y" || overwrite == "Y" {
             fobj = File::create(default_filename).unwrap();
         } else {
-            prompt("Enter new file name: ", None);
+            prompt(PrintArgs { 
+                str_to_print: "Enter new file name: ", 
+                arg: None,  
+                colour: Some(PrintSettings::Colour(Colour::Cyan)) 
+            });
             let new_file_name = input();
             fobj = File::create(new_file_name).unwrap();
         }
